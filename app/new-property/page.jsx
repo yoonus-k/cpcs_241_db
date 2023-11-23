6faa64ae768f4e072b62b487ea0b5c5807e7334a
YOONUS KIZHAKKETHIL <131618962+yoonus-k@users.noreply.github.com>
2023-11-23 23:35:14 +0300

    billing page completed

--- app/new-property/page.jsx
+++ app/new-property/page.jsx
@@ -152,151 +152,156 @@ const page = () => {
         <div className="g-6 flex h-full flex-wrap items-center justify-center text-neutral-800 dark:text-neutral-200">
           <div className="w-full">
             <div className="block rounded-lg bg-white shadow-lg dark:bg-neutral-800">
-              <div className="g-0 ">
-                <div defaultValue="buyer" className="w-full p-10  text-center">
-                  <form onSubmit={handleNewProperty}>
-                    {/* propery type intput */}
-                    <TESelect
-                      required
-                      label="Property Type ?"
-                      data={property_types}
-                      onValueChange={(e) => {
-                        // get the company id from the companies array
-                        setProperty({
-                          ...property,
-                          property_type: e.text,
-                        });
-                      }}
-                      className="mb-4"
-                    />
-                    {/* <!--Price  input--> */}
-                    <TEInput
-                      required
-                      type="number"
-                      label="Price (SAR)"
-                      className="mb-4"
-                      onChange={(e) => {
-                        setProperty({
-                          ...property,
-                          price: e.target.value,
-                        });
-                      }}
-                      max={1000000000}
-                      min={1000000}
-                    ></TEInput>
-
-                    {/* number bedroom, bathroom and garage */}
-                    <TESelect
-                      data={bedrooms_bathrooms}
-                      size="sm"
-                      label="# Bedrooms"
-                      className="mb-4"
-                      onValueChange={(value) => {
-                        setProperty({
-                          ...property,
-                          bedrooms: value.value,
-                        });
-                      }}
-                      preventFirstSelection
-                    />
-                    <TESelect
-                      data={bedrooms_bathrooms}
-                      size="sm"
-                      label="# Bathrooms"
-                      className="mb-4"
-                      onValueChange={(value) => {
-                        setProperty({
-                          ...property,
-                          bathrooms: value.value,
-                        });
-                      }}
-                      preventFirstSelection
-                    />
-                    <TESelect
-                      data={garages}
-                      size="sm"
-                      label="# Garages"
-                      className="mb-4"
-                      onValueChange={(value) => {
-                        setProperty({
-                          ...property,
-                          garages: value.value,
-                        });
-                      }}
-                      preventFirstSelection
-                    />
-
-                    {/* <!--Area input input--> */}
-
-                    <TEInput
-                      type="number"
-                      label="Area in sqm"
-                      className="mb-4"
-                      onChange={(e) => {
-                        setProperty({
-                          ...property,
-                          area: e.target.value,
-                        });
-                      }}
-                      max={10000000}
-                      required
-                    ></TEInput>
-
-                    {/* location input */}
-
-                    <TEInput
-                      type="text"
-                      label="Location"
-                      className="mb-4"
-                      onChange={(e) => {
-                        setProperty({
-                          ...property,
-                          location: e.target.value,
-                        });
-                      }}
-                      required
-                    ></TEInput>
-
-                    {/* <!--description input textarea--> */}
-                    <div className="relative mb-6">
-                      <TETextarea
-                        id="exampleFormControlTextarea13"
-                        label="Description"
-                        rows={3}
+              <div className="g-0 flex justify-around ">
+                <div className="px-4 md:px-0 lg:w-6/12">
+                  <div
+                    defaultValue="buyer"
+                    className="w-full p-10  text-center"
+                  >
+                    <form onSubmit={handleNewProperty}>
+                      {/* propery type intput */}
+                      <TESelect
+                        required
+                        label="Property Type ?"
+                        data={property_types}
+                        onValueChange={(e) => {
+                          // get the company id from the companies array
+                          setProperty({
+                            ...property,
+                            property_type: e.text,
+                          });
+                        }}
+                        className="mb-4"
+                      />
+                      {/* <!--Price  input--> */}
+                      <TEInput
+                        required
+                        type="number"
+                        label="Price (SAR)"
+                        className="mb-4"
                         onChange={(e) => {
                           setProperty({
                             ...property,
-                            description: e.target.value,
+                            price: e.target.value,
                           });
                         }}
+                        max={1000000000}
+                        min={1000000}
+                      ></TEInput>
+
+                      {/* number bedroom, bathroom and garage */}
+                      <TESelect
+                        data={bedrooms_bathrooms}
+                        size="sm"
+                        label="# Bedrooms"
+                        className="mb-4"
+                        onValueChange={(value) => {
+                          setProperty({
+                            ...property,
+                            bedrooms: value.value,
+                          });
+                        }}
+                        preventFirstSelection
                       />
-                    </div>
+                      <TESelect
+                        data={bedrooms_bathrooms}
+                        size="sm"
+                        label="# Bathrooms"
+                        className="mb-4"
+                        onValueChange={(value) => {
+                          setProperty({
+                            ...property,
+                            bathrooms: value.value,
+                          });
+                        }}
+                        preventFirstSelection
+                      />
+                      <TESelect
+                        data={garages}
+                        size="sm"
+                        label="# Garages"
+                        className="mb-4"
+                        onValueChange={(value) => {
+                          setProperty({
+                            ...property,
+                            garages: value.value,
+                          });
+                        }}
+                        preventFirstSelection
+                      />
 
-                    {/* photo upload input */}
+                      {/* <!--Area input input--> */}
 
-                    <input
-                      className="mb-4 m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
-                      type="file"
-                      id="formFile"
-                      ref={imageRef}
-                    />
+                      <TEInput
+                        type="number"
+                        label="Area in sqm"
+                        className="mb-4"
+                        onChange={(e) => {
+                          setProperty({
+                            ...property,
+                            area: e.target.value,
+                          });
+                        }}
+                        max={10000000}
+                        required
+                      ></TEInput>
 
-                    {/* <!--Submit button--> */}
-                    <div className="mb-12 pb-1 pt-1 text-center">
-                      <TERipple rippleColor="light" className="w-full">
-                        <button
-                          className="mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
-                          type="submit"
-                          style={{
-                            background:
-                              "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)",
+                      {/* location input */}
+
+                      <TEInput
+                        type="text"
+                        label="Location"
+                        className="mb-4"
+                        onChange={(e) => {
+                          setProperty({
+                            ...property,
+                            location: e.target.value,
+                          });
+                        }}
+                        required
+                      ></TEInput>
+
+                      {/* <!--description input textarea--> */}
+                      <div className="relative mb-6">
+                        <TETextarea
+                          id="exampleFormControlTextarea13"
+                          label="Description"
+                          rows={3}
+                          onChange={(e) => {
+                            setProperty({
+                              ...property,
+                              description: e.target.value,
+                            });
                           }}
-                        >
-                          + add property
-                        </button>
-                      </TERipple>
-                    </div>
-                  </form>
+                        />
+                      </div>
+
+                      {/* photo upload input */}
+
+                      <input
+                        className="mb-4 m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
+                        type="file"
+                        id="formFile"
+                        ref={imageRef}
+                      />
+
+                      {/* <!--Submit button--> */}
+                      <div className="mb-12 pb-1 pt-1 text-center">
+                        <TERipple rippleColor="light" className="w-full">
+                          <button
+                            className="mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
+                            type="submit"
+                            style={{
+                              background:
+                                "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)",
+                            }}
+                          >
+                            + add property
+                          </button>
+                        </TERipple>
+                      </div>
+                    </form>
+                  </div>
                 </div>
               </div>
             </div>
